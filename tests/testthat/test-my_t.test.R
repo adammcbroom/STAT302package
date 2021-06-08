7b67860701fbf9a15cec7848a17d49086f4d5f7a
Adam McBroom <ads15mcb@gmail.com>
2021-06-08 12:50:46 -0700

    Add unit testing for my_t.test

--- tests/testthat/test-my_t.test.R
+++ tests/testthat/test-my_t.test.R
@@ -1,7 +1,10 @@
 test_that("incorrect 'alternative' input throws error", {
-  expect_error(my_t.test(x, alternative = "both.sided"))
+  expect_error(my_t.test(1:10000, alternative = "both.sided", mu = 4900))
 })
 
 test_that("my_t.test returns a list", {
   expect_type(my_t.test(1:10000, alternative = "two.sided", mu = 4900), "list")
+  expect_type(my_t.test(1:10000, alternative = "less", mu = 4900), "list")
+  expect_type(my_t.test(1:10000, alternative = "greater", mu = 5100), "list")
 })
+
