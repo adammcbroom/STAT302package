2fcd9d5e0b1d69ab68b080c55adfda11d54746b1
Adam McBroom <ads15mcb@gmail.com>
2021-06-08 13:14:23 -0700

    Add more unit testing for my_lm

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,3 +1,8 @@
 test_that("my_lm returns a list", {
   expect_type(my_lm(lifeExp ~ gdpPercap, my_gapminder), "list")
 })
+
+test_that("missing arguments return an error", {
+  expect_error(my_lm(lifeExp ~ gdpPercap))
+  expect_error(my_lm(gapminder))
+})
